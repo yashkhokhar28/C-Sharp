fa3e4d7d326a82ebf0b8e53ce8590c5285b50cc8
Yash Khokhar <yashkhokhar28@gmail.com>
2023-07-02 14:51:05 +0530

    Add files via upload

---
+++ 4_exception_handling/sumUsingAbstractClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Result r = new Result();
+            Console.WriteLine("Enter First Number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum Of " + num1 + " and " + num2 + " is " + r.sumOfTwo(num1, num2));
+            Console.WriteLine("Enter Third Number : ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum Of " + num1 + "," + num2 + " and " + num3 + " is " + r.sumOfThree(num1, num2, num3));
+
+        }
+    }
+
+    abstract class Sum
+    {
+        public abstract int sumOfTwo(int a, int b);
+        public abstract int sumOfThree(int a, int b, int c);
+    }
+
+    class Result : Sum
+    {
+        public override int sumOfTwo(int a, int b)
+        {
+            return a + b;
+        }
+
+        public override int sumOfThree(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
