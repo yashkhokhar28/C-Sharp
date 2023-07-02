985fc2d93e86a3edb00e0376ad97a412738efe0a
Yash Khokhar <yashkhokhar28@gmail.com>
2023-07-02 14:28:18 +0530

    Add files via upload

---
+++ 4_exception_handling/indexOutOfRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+                Console.WriteLine("Enter Number : ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter Index Of Number You Want : ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Your Element : " + arr[index]);   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ 4_exception_handling/oddNumberException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("Even Number : " + num);
+                }
+                else
+                {
+                    throw new OddNumberException("Not Even Number");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    class OddNumberException : Exception
+    {
+        public OddNumberException(String message) : base(message) { }
+
+    }
+}
