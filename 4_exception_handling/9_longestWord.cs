1077f437d10d43aacb34924044e5ade0961f087d
Yash Khokhar <yashkhokhar28@gmail.com>
2023-07-17 18:44:48 +0530

    Update 9_longestWord.cs

--- 4_exception_handling/9_longestWord.cs
+++ 4_exception_handling/9_longestWord.cs
@@ -11,15 +11,20 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number : ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Number : ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            num1 = num1 + num2;
-            num2 = num1 - num2;
-            num1 = num1 - num2;
-            Console.WriteLine("Number 1 : " + num1);
-            Console.WriteLine("Number 2 : " + num2);
+            Console.Write("Enter a string : ");
+            string str = Console.ReadLine();
+    
+            List<String> wordList = str.Split(" ").ToList();
+            string longestWord = "";
+    
+            foreach (String word in wordList)
+            {
+                if (longestWord.Length < word.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine("Longest word is : "+longestWord);
         }
     }
 }
